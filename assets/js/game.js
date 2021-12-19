2f6122982e5caa9759a8f77b37d6a878b21d209a
Brandonpeterson6 <brandonpeterson.006@gmail.com>
2021-12-18 21:26:50 -0700

    started learning JS codes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! Javascript is running!")
+var playername = window.prompt("What is your robot's name?");
+console.log(playername);
+console.log(10+10);
+console.log("Our robot's name is" + playername);
+
+var name ="your name";
+console.log("name")
+
+function fight() {
+    window.alert("the fight has begun!");
+}
+fight();
